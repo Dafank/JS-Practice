0710376fdb1f5612358a1e0306048bf1cd9eb126
Dafank <fentik9814@gmail.com>
2020-03-31 22:01:48 +0300

    first js project

---
+++ script.js
@@ -0,0 +1,33 @@
+'use strict'
+// let company = 
+// {
+//     name: "Microsoft",
+//     str: "smesdad",
+//     phone: "dwadawd"
+// }
+// console.log(company.phone);
+
+// let arr = [true,false,true];
+// console.log(arr[1]);
+
+// let answer = confirm("Are you adult?");
+// console.log(answer);
+
+// let answer = prompt("How many cats do you have?","no one");
+
+// console.log("\""+answer + "\" - "+typeof(answer));
+
+// налаштувати go live
+
+let left = 9,
+    right = 9;
+
+console.log(++left);
+console.log(right++);
+
+
+let str = +'dwad';
+
+console.log(str === +"dawd"); //false
+console.log(typeof (str) === typeof (+"dawd")); //true
+console.log(+"dawd");
